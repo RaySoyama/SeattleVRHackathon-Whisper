5ef0097b27ff627797dae8aa5596d0c7ecf7fe0a
unknown <s179069@students.aie.edu.au>
2019-06-22 09:13:34 -0700

    Create audio branch

---
+++ Whisper/Assets/_Whisper/_Scripts/AudioHandling/MicHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class MicHandler : MonoBehaviour
+{
+
+    [SerializeField] private AudioSource audioSource;
+
+
+
+    private void Reset()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void Awake()
+    {
+        Debug.Log(Microphone.devices);
+        //audioSource.clip = Microphone.Start()
+    }
+
+}
